98bdda9278c72565c3bbb20d1d04bb6661802115
ThorpeZhang <1027285564@qq,com>
2019-12-18 15:37:49 +0800

    在此修复了导致docker环境下无法编译的bug

--- instruction/instruction.h
+++ instruction/instruction.h
@@ -31,16 +31,14 @@ namespace c0 {
 	};
 	
 	class Instruction final {
-    public:
-	    int32_t opn;
 	private:
-		//using int32_t = std::int32_t;
+		using int32_t = std::int32_t;
 	public:
 		friend void swap(Instruction& lhs, Instruction& rhs);
 	public:
-        Instruction(Operation opr) :opn(0),_opr(opr), _x(-1), _option(-1) {}
-		Instruction(Operation opr, int32_t x) : opn(0),_opr(opr), _x(x), _option(-1) {}
-        Instruction(Operation opr, int32_t x, int32_t option) : opn(0),_opr(opr), _x(x), _option(option) {}
+        Instruction(Operation opr) :_opr(opr), _x(-1), _option(-1) {}
+		Instruction(Operation opr, int32_t x) : _opr(opr), _x(x), _option(-1) {}
+        Instruction(Operation opr, int32_t x, int32_t option) : _opr(opr), _x(x), _option(option) {}
 		
 		Instruction() : Instruction(Operation::ILL, 0){}
 		Instruction(const Instruction& i) { _opr = i._opr; _x = i._x; _option = i._option; }
